c3245d1b88040afe70bee5a63e9659111e4203e2
new010330 <rlarudtjs74a@naver.com>
2023-07-18 02:51:00 +0900

    지도 페이지 html, js 작업중

--- src/main/resources/static/js/search-map.js
+++ src/main/resources/static/js/search-map.js
@@ -1,9 +1,44 @@
-const optionButtons = document.querySelectorAll(".option-button");
+const optionBoxes = document.querySelectorAll(".option-box");
+const buttons = document.querySelectorAll(".search-type button");
+const optionDetailLabel = document.querySelectorAll(".option-detail-label");
 
-for(let i = 0; i < optionButtons.length; i++) {
+buttons[0].onclick = () => {
+    if(buttons[1].classList.contains("button-color")) {
+        buttons[1].classList.remove("button-color");
+        buttons[0].classList.add("button-color");
 
-    optionButtons[i].onclick = () => {
-        optionButtons[i].classList.toggle("black-button");
-        optionButtons[i].classList.toggle("blue-button");
     }
 }
+
+buttons[1].onclick = () => {
+    if(buttons[0].classList.contains("button-color")) {
+        buttons[0].classList.remove("button-color");
+        buttons[1].classList.add("button-color");
+    }
+}
+
+optionBoxes.forEach((optionBox) => {
+    const optionButton = optionBox.querySelector(".option-button");
+    const textColor = optionButton.querySelector("p");
+    const optionContainer = optionBox.querySelector(".option-detail-container");
+  
+    optionButton.onclick = () => {
+      optionButton.classList.toggle("black-button");
+      optionButton.classList.toggle("blue-button");
+      textColor.classList.toggle("black-font");
+      textColor.classList.toggle("blue-font");
+      optionContainer.classList.toggle("hidden");
+
+optionDetailLabels.forEach((optionLabel) => {
+    const optionInput = optionLabel.querySelector(".option-detail-input");
+    const checkText = optionLabel.parentNode.querySelector(".option-button p");
+    optionLabel.onclick = () => {
+        if (optionInput.checked) {
+            checkText.textContent = optionInput.value;
+        } else {
+            checkText.textContent = ""; // 체크되지 않은 경우 텍스트를 비웁니다.
+        }
+    };
+});
+    };
+});
