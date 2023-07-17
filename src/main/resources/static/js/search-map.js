1cbdcdca47ee5b4737ab6b366fd9a88d8dab2353
new010330 <rlarudtjs74a@naver.com>
2023-07-17 23:15:26 +0900

    지도 페이지 작성중

--- src/main/resources/static/js/search-map.js
+++ src/main/resources/static/js/search-map.js
@@ -1,2 +1,9 @@
 const optionButtons = document.querySelectorAll(".option-button");
 
+for(let i = 0; i < optionButtons.length; i++) {
+
+    optionButtons[i].onclick = () => {
+        optionButtons[i].classList.toggle("black-button");
+        optionButtons[i].classList.toggle("blue-button");
+    }
+}
