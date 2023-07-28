e9a5ffa317049b513ae8392a949996235cbc2aa3
kimheyonjin <ropotsgames@gmail.com>
2023-07-28 21:51:03 +0900

    button js

--- src/main/resources/static/js/search-map.js
+++ src/main/resources/static/js/search-map.js
@@ -17,7 +17,7 @@ buttons[1].onclick = () => {
     }
 }
 
-optionBoxes.forEach((optionBox) => {
+/*optionBoxes.forEach((optionBox) => {
     const optionButton = optionBox.querySelector(".option-button");
     const textColor = optionButton.querySelector("p");
     const optionContainer = optionBox.querySelector(".option-detail-container");
@@ -37,6 +37,50 @@ optionBoxes.forEach((optionBox) => {
         textColor.classList.toggle("black-font");
         textColor.classList.toggle("blue-font");
         optionContainer.classList.toggle("hidden");
+      }
+    }
+});*/
+
+optionBoxes.forEach((optionBox) => {
+    const optionButton = optionBox.querySelector(".option-button");
+    const textColor = optionButton.querySelector("p");
+    const optionContainer = optionBox.querySelector(".option-detail-container");
+    hiddenFlag = true;
+	
+    optionButton.onclick = () => {
+      optionButton.classList.toggle("black-button");
+      optionButton.classList.toggle("blue-button");
+      textColor.classList.toggle("black-font");
+      textColor.classList.toggle("blue-font");
+      optionContainer.classList.toggle("hidden");
+      optionButton.focus();
+      optionContainer.focus();
+      hiddenFlag = false;
+      console.log(document.activeElement === optionButton);
+      
+      
+      optionButton.onblur = () => {
+	      if(!optionContainer.classList.contains("hidden")) {
+	        optionButton.classList.toggle("black-button");
+	        optionButton.classList.toggle("blue-button");
+	        textColor.classList.toggle("black-font");
+	        textColor.classList.toggle("blue-font");
+	        optionContainer.classList.toggle("hidden");
+	        optionButton.blur();
+	        optionContainer.blur();
+	      }
+	   }
+    }
+
+    optionContainer.onblur = () => {
+      if (!optionContainer.classList.contains('hidden')) {
+        optionButton.classList.toggle("black-button");
+        optionButton.classList.toggle("blue-button");
+        textColor.classList.toggle("black-font");
+        textColor.classList.toggle("blue-font");
+        optionContainer.classList.toggle("hidden");
+        optionButton.blur();
+	    optionContainer.blur();
       }
     }
 });
