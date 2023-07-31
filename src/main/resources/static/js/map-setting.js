d7782fc82e23a3e8f65f6e1c4063cf4f45a7b466
kimheyonjin <ropotsgames@gmail.com>
2023-07-31 21:51:56 +0900

    수정

--- src/main/resources/static/js/map-setting.js
+++ src/main/resources/static/js/map-setting.js
@@ -38,6 +38,19 @@ let container = document.getElementById('map');
             }]
         });
 
+        $.get("/download/web/data/chicken.json", function(data) {
+            // 데이터에서 좌표 값을 가지고 마커를 표시합니다
+            // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
+            var markers = $(data.positions).map(function(i, position) {
+                return new kakao.maps.Marker({
+                    position : new kakao.maps.LatLng(position.lat, position.lng)
+                });
+            });
+    
+            // 클러스터러에 마커들을 추가합니다
+            clusterer.addMarkers(markers);
+        });
+
         // let customOverlay = new kakao.maps.CustomOverlay({
         //     map: map,
         //     clickable: true,
